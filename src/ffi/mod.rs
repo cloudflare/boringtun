52d5573def4b301771845170577de74885df187a
Kornel Lesiński <kornel@cloudflare.com>
2019-02-18 14:43:39 +0000

    Don't override C enum size

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -13,7 +13,7 @@ use std::slice;
 use std::sync::Arc;
 
 #[allow(non_camel_case_types)]
-#[repr(u32)]
+#[repr(C)]
 /// Indicates the operation required from the caller
 pub enum result_type {
     /// No operation is required.
