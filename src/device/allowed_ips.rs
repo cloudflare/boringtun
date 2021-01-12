d513497793b4cf9bf981fb2d4531e0289b5ee422
Mikael Magnusson <mikma@users.sourceforge.net>
2021-01-12 02:09:26 +0100

    Fix AllowedIps iteration with empty leaves (#156)
    Avoid right shift overflow when a leaf doesn't contain any bits.

--- src/device/allowed_ips.rs
+++ src/device/allowed_ips.rs
@@ -451,6 +451,9 @@ macro_rules! build_iter {
                         }
                         Some($node::Leaf(key, bits, data)) => {
                             let (cur_key, cur_bits) = self.key_hlp.pop().unwrap();
+                            if cur_bits == BITS && *bits == 0 {
+                                return Some((data, cur_key, cur_bits));
+                            }
                             return Some((data, cur_key ^ (key >> cur_bits), cur_bits + bits));
                         }
                         Some($node::Node {
@@ -775,5 +778,28 @@ mod tests {
                 0x2404, 0x6800, 0x4004, 0x0800, 0xdead, 0xbeef, 0xdead, 0xbeef
             ]))
         );
+    }
+
+    #[test]
+    fn test_allowed_ips_iter_zero_leaf_bits() {
+        let mut map: AllowedIps<char> = Default::default();
+        map.insert(IpAddr::from([10, 111, 0, 1]), 32, '1');
+        map.insert(IpAddr::from([10, 111, 0, 2]), 32, '2');
+        map.insert(IpAddr::from([10, 111, 0, 3]), 32, '3');
+
+        let mut map_iter = map.iter();
+        assert_eq!(
+            map_iter.next(),
+            Some((&'1', IpAddr::from([10, 111, 0, 1]), 32))
+        );
+        assert_eq!(
+            map_iter.next(),
+            Some((&'2', IpAddr::from([10, 111, 0, 2]), 32))
+        );
+        assert_eq!(
+            map_iter.next(),
+            Some((&'3', IpAddr::from([10, 111, 0, 3]), 32))
+        );
+        assert_eq!(map_iter.next(), None);
     }
 }
