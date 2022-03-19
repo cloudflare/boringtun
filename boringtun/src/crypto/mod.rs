f2ea1a3778f5bd832d1b00eeb61e06b30617e697
Sean Madden <sean@seanmadden.net>
2022-03-18 23:35:31 -0400

    Fix rustfmt issue

--- boringtun/src/crypto/mod.rs
+++ boringtun/src/crypto/mod.rs
@@ -9,7 +9,7 @@ mod x25519;
 
 pub use blake2s::{constant_time_mac_check, Blake2s};
 pub use chacha20poly1305::ChaCha20Poly1305;
-pub use x25519::{X25519PublicKey, X25519SecretKey};
 pub use chacha20poly1305::Poly1305;
+pub use x25519::{X25519PublicKey, X25519SecretKey};
 
 pub use ring::rand::SystemRandom;
