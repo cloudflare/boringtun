b084e9ec43ba364318e2a5ad67e58096784fcfb4
Joakim Hulthe <joakim.hulthe@mullvad.net>
2025-07-28 17:30:16 +0200

    Decuple PcapWriter from PcapSniffer

--- boringtun/src/tun/pcap.rs
+++ boringtun/src/tun/pcap.rs
@@ -1,25 +1,33 @@
+//! See [PcapSniffer].
+
 use std::{
     io::{self, Write},
     sync::Arc,
     time::Instant,
 };
 
-use pcap_file::pcap::{PcapHeader, PcapPacket, PcapWriter};
+use pcap_file::pcap::{PcapHeader, PcapPacket};
 use tokio::sync::Mutex;
 
 use crate::tun::{IpRecv, IpSend};
 
-/// An implementor of [IpSend] and [IpRecv] which also dumps all packets in the pcap file format to a [Write].
 #[derive(Clone)]
-pub struct PcapSniff<I> {
-    inner: I,
-    epoch: Instant,
-    write: Arc<Mutex<PcapWriter<Box<dyn Write + Send>>>>,
+pub struct PcapStream {
+    writer: Arc<Mutex<pcap_file::pcap::PcapWriter<Box<dyn Write + Send>>>>,
 }
 
-impl<R> PcapSniff<R> {
-    pub fn new(inner: R, write: Box<dyn Write + Send>, epoch: Instant) -> Self {
-        let writer = PcapWriter::with_header(
+
+/// An implementor of [IpSend] and [IpRecv] which also dumps all packets in the pcap file format to a [Write] (See [PcapStream]).
+#[derive(Clone)]
+pub struct PcapSniffer<I> {
+    inner: I,
+    epoch: Instant,
+    writer: PcapStream,
+}
+
+impl PcapStream {
+    pub fn new(write: Box<dyn Write + Send>) -> Self {
+        let writer = pcap_file::pcap::PcapWriter::with_header(
             write,
             PcapHeader {
                 endianness: pcap_file::Endianness::native(),
@@ -28,15 +36,24 @@ impl<R> PcapSniff<R> {
             },
         )
         .unwrap();
+
         Self {
-            inner,
-            epoch,
-            write: Arc::new(Mutex::new(writer)),
+            writer: Arc::new(Mutex::new(writer)),
         }
     }
 }
 
-impl<R: IpRecv> IpRecv for PcapSniff<R> {
+impl<R> PcapSniffer<R> {
+    pub fn new(inner: R, writer: PcapStream, epoch: Instant) -> Self {
+        Self {
+            inner,
+            epoch,
+            writer,
+        }
+    }
+}
+
+impl<R: IpRecv> IpRecv for PcapSniffer<R> {
     async fn recv(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let n = self.inner.recv(buf).await?;
 
@@ -44,20 +61,20 @@ impl<R: IpRecv> IpRecv for PcapSniff<R> {
 
         let timestamp = Instant::now().duration_since(self.epoch);
         let pcap_packet = PcapPacket::new(timestamp, packet.len() as u32, packet);
-        let mut write = self.write.lock().await;
+        let mut write = self.writer.writer.lock().await;
         let _ = write.write_packet(&pcap_packet);
 
         Ok(n)
     }
 }
 
-impl<S: IpSend> IpSend for PcapSniff<S> {
+impl<S: IpSend> IpSend for PcapSniffer<S> {
     async fn send(&self, packet: &[u8]) -> io::Result<()> {
         self.inner.send(packet).await?;
 
         let timestamp = Instant::now().duration_since(self.epoch);
         let pcap_packet = PcapPacket::new(timestamp, packet.len() as u32, packet);
-        let mut write = self.write.lock().await;
+        let mut write = self.writer.writer.lock().await;
         let _ = write.write_packet(&pcap_packet);
 
         Ok(())
